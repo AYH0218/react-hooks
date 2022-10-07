2b17e1c5505dad4651dea6a642a5048ef084d9bb
AYH0218 <ayuhak0218@gmail.com>
2022-10-07 19:21:41 +0900

    useEffect,Ref,Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react';
+import { useContext, useEffect, useReducer, useRef, useState } from 'react';
 import './App.css';
+import PersonalInfoContext from './main';
+
+//useReducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'incr':
+      return state + 1;
+    case 'decr':
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 function App() {
   const [count, setCount] = useState(0);
   const [timesNum, setTimesNum] = useState(2);
   const [divideNum, setDivideNum] = useState(2);
+  const personalInfo = useContext(PersonalInfoContext);
+  const ref = useRef();
+  const [state, dispatch] = useReducer(reducer, 0);
 
+  //useState
   const increment = () => {
     setCount(count + 1);
   };
@@ -26,9 +43,24 @@ function App() {
     setCount(0);
   };
 
+  // useEffect
+  useEffect(() => {
+    console.log('Hello Hooks');
+    // setCount(count + 1); //無限ループ
+  }, [count]);
+  //  ↑に変更があったときに発火
+
+  // useContext 階層を跨いで直接渡せる
+
+  //useRef 指定した要素の値を見る
+  const handleRef = () => {
+    console.log(ref.current.value);
+    console.log(ref.current.offsetHeight);
+  };
+
   return (
     <div className="App">
-      <h1>UseState</h1>
+      <h1>useState, useEffect</h1>
       <p>{count}</p>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
@@ -39,6 +71,29 @@ function App() {
       <br />
       <button onClick={divide}>÷{divideNum}</button>
       <input className="area" type="number" size="2" value={divideNum} onChange={(e) => setDivideNum(e.target.value)}></input>
+
+      <hr />
+
+      <h1>useContext</h1>
+      <p>{personalInfo.name}</p>
+      <p>{personalInfo.age}</p>
+
+      <hr />
+
+      <h1>useRef</h1>
+      <input type="text" ref={ref} />
+      <button onClick={handleRef}>useRef</button>
+
+      <hr />
+
+      <h1>useReducer</h1>
+      <p>カウント：{state}</p>
+      <button onClick={() => dispatch({ type: 'incr' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decr' })}>-</button>
+
+      <hr />
+
+      <h1>useMemo</h1>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React, { createContext } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const personalInfo = {
+  name: 'User Name',
+  age: 20,
+};
+// useContext 階層を跨いで直接渡せる
+const PersonalInfoContext = createContext(personalInfo);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+  <PersonalInfoContext.Provider value={personalInfo}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </PersonalInfoContext.Provider>
+);
+
+export default PersonalInfoContext;
